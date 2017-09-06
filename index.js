c3f348c3ae737449a58cf30bf168ed55e1f966b7
Donte Hightower <dontehightower12@gmail.com>
2017-09-06 16:07:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,22 +9,48 @@ function setCart(c) {
   return cart;
 }
 
-function addToCart(item) {
+function addToCart(string) {
+  console.log(`${string} has been added to your cart.`)
+  var string = {[string]: Math.floor(Math.random() * 100)}
+  cart.push(string)
+  if (cart.length > 0) {
+    return cart
+  }
  // write your code here
 }
 
 function viewCart() {
+  var cartTotal =  cart[0] + cart[i]
   // write your code here
 }
 
 function total() {
-  // write your code here
+    var total = 0
+    for (let i = 0; i < cart.length; i++) {
+      total += cart[i][Object.keys(cart[i])[0]]
+  }
+   return total
 }
 
-function removeFromCart(item) {
   // write your code here
+
+function removeFromCart(string) {
+  for (let i = 0; cart.length > i; i++) {
+    if (cart[i].hasOwnProperty(string)) {
+      cart.splice(i, 1)
+    } else if (cart.hasOwnProperty(string) === false) {
+      console.log(`That item is not in your cart.`)}
+  }
 }
+  // write your code here
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (cardNumber === NaN)
+    { console.log (`Sorry, we don't have a credit card on file for you.`)
+  }  else {console.log (`Your total cost is $${total()}, which will be be charged to the card ${cardNumber}.`)
+  }
+  while (cart.length > 0 ) {
+    cart.push(cart[0])
+  }
+  return cart
 }
